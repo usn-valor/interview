85d0bca8f408e8c1d568738920f8559e2bc3f033
Arsen Sayfullin <silver.arsen@gmail.com>
2021-07-12 22:48:42 +0300

    Add Homework to Lesson 1

---
+++ Lesson_1/LightWeightCar.java
@@ -0,0 +1,81 @@
+package Interview.Lesson_1;
+
+interface Moveable {
+
+    void move();
+}
+
+interface Stopable {
+
+    void stop();
+}
+
+abstract class Car {
+    public Engine engine;
+    private String color;
+    private String name;
+
+    protected void start() {
+        System.out.println("Car starting");
+    }
+
+    abstract void open();
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+//    public void setEngine(Engine engine) {
+//        this.engine = engine;
+//    }
+
+    public String getColor() {
+        return color;
+    }
+
+//    public void setColor(String color) {
+//        this.color = color;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
+    private static class Engine {
+
+    }
+}
+
+
+class LightWeightCar extends Car implements Moveable{
+
+    @Override
+    void open() {
+        System.out.println("Car is open");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Car is moving");
+    }
+}
+
+class Lorry extends Car implements Moveable, Stopable{
+
+    public void move(){
+        System.out.println("Car is moving");
+    }
+
+    public void stop(){
+        System.out.println("Car is stop");
+    }
+
+    @Override
+    void open() {
+
+    }
+}
---
+++ Lesson_1/Shape.java
@@ -0,0 +1,42 @@
+package Interview.Lesson_1;
+
+public class Shape {
+
+    public void draw() {
+        System.out.println("Draw Shape");
+    }
+}
+
+class Circle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Draw Circle");
+    }
+}
+
+class Square extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Draw Square");
+    }
+}
+
+class Triangle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Draw Triangle");
+    }
+}
+
+class Test {
+
+    public static void main(String[] args) {
+        Shape shape = new Circle();
+        testShape(shape);
+    }
+
+    private static void testShape(Shape shape) {
+        shape.draw();
+    }
+}
+
